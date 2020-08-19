bb5ea5165e8fab262e267cee0f24b4e43fb3c089
Nikhil Pimpalkhare <nikhilpimpalkhare@Nikhils-MacBook-Pro.local>
2020-08-18 18:26:58 -0700

    added full test script

---
+++ full_test.sh
@@ -0,0 +1,6 @@
+medley ./tmp/ nearest.csv -c neighbor
+medley ./tmp/ random.csv -c random
+medley ./tmp/ neural.csv -c MLP
+medley ./tmp/ thompson.csv -c thompson
+medley ./tmp/ linucb.csv -c linear
+
