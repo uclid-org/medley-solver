2408519f64a06e9ff2681b070a70b6bb0c5897a9
Nikhil Pimpalkhare <nikhilpimpalkhare@Nikhils-MacBook-Pro.local>
2020-10-08 23:10:29 -0700

    count overhead

--- medleysolver/runner.py
+++ medleysolver/runner.py
@@ -1,4 +1,5 @@
 import numpy as np, csv, random, tqdm
+import time
 from collections import OrderedDict
 from medleysolver.compute_features import get_features
 from medleysolver.constants import SOLVERS, Result, Solved_Problem, SAT_RESULT, UNSAT_RESULT, is_solved
@@ -10,15 +11,16 @@ def execute(problems, output, classifier, time_manager, timeout):
     writer = csv.writer(open(output, 'w'))
 
     for c, prob in tqdm.tqdm(enumerate(problems, 1)): 
+        start = time.time()
         point = np.array(get_features(prob))
         #normalizing point
         mean = (c - 1) / c * mean + 1 / c * point
         point = point / (mean+1e-9)
 
         order = classifier.get_ordering(point, c)
-
-        solver, elapsed, result, rewards = apply_ordering(prob, order, timeout, time_manager)
-        solved_prob = Solved_Problem(prob, point, solver, elapsed, result)
+        end = time.time()
+        solver, elapsed, result, rewards = apply_ordering(prob, order, timeout - (end - start), time_manager)
+        solved_prob = Solved_Problem(prob, point, solver, elapsed + (end - start), result)
 
         classifier.update(solved_prob, rewards)
 
