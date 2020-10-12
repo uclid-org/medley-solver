6af5a56a23ba989a75f2e5eb73ecd898c73d49dc
FedericoAureliano <fmorarocha@gmail.com>
2020-10-11 22:57:20 -0700

    binary rewards and timeout at the end for first choice solver

--- medleysolver/runner.py
+++ medleysolver/runner.py
@@ -33,15 +33,17 @@ def apply_ordering(problem, order, timeout, time_manager):
     rewards = [-1 for _ in SOLVERS] # negative rewards should be ignored. 
     time_spent = []
     for solver in order:
-        if solver == order[-1]:
-            time_for_solver = timeout - elapsed
-        else:
-            time_for_solver = time_manager.get_timeout(solver)
+        # if solver == order[-1]:
+        #     time_for_solver = timeout - elapsed
+        # else:
+        #     time_for_solver = time_manager.get_timeout(solver)
+        time_for_solver = time_manager.get_timeout(solver)
         time_for_solver = min(time_for_solver, timeout - elapsed)
         
         res = run_problem(solver, SOLVERS[solver], problem, time_for_solver)
 
-        reward = (1 - res.elapsed / timeout) ** 4 if is_solved(res.result) else 0
+        # reward = (1 - res.elapsed / timeout) ** 4 if is_solved(res.result) else 0
+        reward = 1 if is_solved(res.result) else 0
         rewards[list(SOLVERS.keys()).index(solver)] = reward
         time_spent.append(res.elapsed)
 
@@ -49,5 +51,18 @@ def apply_ordering(problem, order, timeout, time_manager):
         time_manager.update(solver, res.elapsed, is_solved(res.result))
         if elapsed >= timeout or is_solved(res.result):
             break
+
+    # if theres any time leftover, use it for the first solver
+    if elapsed < timeout and not is_solved(res.result):
+        time_for_solver = timeout - elapsed
+        res = run_problem(order[0], SOLVERS[order[0]], problem, time_for_solver)
+
+        # reward = (1 - res.elapsed / timeout) ** 4 if is_solved(res.result) else 0
+        reward = 1 if is_solved(res.result) else 0
+        rewards[list(SOLVERS.keys()).index(solver)] = reward
+        time_spent.append(res.elapsed)
+
+        elapsed += res.elapsed
+        time_manager.update(solver, res.elapsed, is_solved(res.result))
 
     return solver, elapsed, res.result, rewards, time_spent
