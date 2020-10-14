ae7d563fcf1535d248b53ef7c2edd64cdcc15451
FedericoAureliano <fmorarocha@gmail.com>
2020-10-13 18:17:48 -0700

    don't run exp3 with 0.5

--- machine1.sh
+++ machine1.sh
@@ -1,4 +1,4 @@
-for learner in thompson neighbor knearest random MLP linear exp3a exp3b exp3c exp3d; do
+for learner in thompson neighbor knearest random MLP linear exp3a exp3b exp3c; do
     sh ./runlearner.sh tmp/BV $learner
     sh ./runlearner.sh tmp/QF_ABV $learner
     sh ./runlearner.sh tmp/QF_AUFBV $learner
